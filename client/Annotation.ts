9bc4d8cc63cf6231c5c12afb92b3151d43d8326b
Frank Filippis <frank@filippis.com>
2016-08-24 19:00:57 -0400

    Push

--- client/Annotation.ts
+++ client/Annotation.ts
@@ -1,21 +1,83 @@
 /// <reference path="../../Video/typescript-defs/all-definitions.d.ts"/>
 
+class VEvent { } this.VEvent = VEvent;
+
+class Story {
+
+    public perspectives : Array<Perspective>;
+    public events : Array<VEvent>;
+    public player : Player;
 
 
-class Perspective {
+    constructor() {
 
-} this.Perspective = Perspective;
+    }
 
+} this.Story = Story;
 
-class VideoPerspective extends Perspective {
+class Player {
 
-} this.VideoPespective = VideoPerspective;
+    constructor() {}
+    public play() {}
+    public stop() {}
+    public pause() {}
+    public getTime() : number {return 0;}
+    public setTime(time : number) {}
 
-class ClipboardPerspective extends Perspective {
+} this.Player = Player;
 
-} this.ClipboardPerspective = ClipboardPerspective;
+class VideoPlayer extends Player {
 
+    public videoJS : any = null;
+    public videoID : string;
+    public videoElement : any;
 
+    constructor ( videoID : string){
+
+        super();
+
+        this.videoID = videoID;
+        this.videoElement = document.getElementById(videoID);
+
+        /*  this.videoElement =
+         videojs(
+         this.videoID ,
+         {},
+         {"controls": true , "autoplay": false,"preload":"auto"},
+         function(){           // Player (this) is initialized and ready.        }
+         }) */
+    }
+
+    public play() {
+        this.videoElement.play();
+    }
+
+    public pause() {
+        this.videoElement.pause();
+    }
+    public stop() {
+        this.videoElement.getElement().stop();
+    }
+
+    public currentTime() : number {
+        var current_time = this.videoElement.currentTime;
+        return current_time;
+    }
+
+    public getTime() : number {
+        return this.currentTime();
+    }
+    public setTime(time : number) {
+        this.videoElement.currentTime(time);
+    }
+
+} this.VideoPlayer = VideoPlayer;
+
+class Perspective { } this.Perspective = Perspective;
+
+class VideoPerspective extends Perspective { } this.VideoPespective = VideoPerspective;
+
+class ClipboardPerspective extends Perspective { } this.ClipboardPerspective = ClipboardPerspective;
 
 class AnnotationBlock {
 
@@ -48,11 +110,7 @@ class AnnotationBlock {
 
 } this.AnnotationBlock = AnnotationBlock;
 
-
-
-class VEvent { }
-this.VEvent = VEvent;
-
+class VideoEvent extends VEvent { } this.VideoEvent = VideoEvent;
 
 class Annotation extends VEvent {
 
@@ -135,7 +193,6 @@ class Annotation extends VEvent {
     }
 
 } this.Annotation = Annotation;
-
 
 class DrawAnnotation extends Annotation {
 
@@ -268,3 +325,167 @@ class TextAnnotation extends DrawAnnotation {
     }
 
 } this.TextAnnotation = TextAnnotation;
+
+class UIPlayerControls extends UIComplexComponent {
+
+    public playButton   : UIButton;
+    public stopButton   : UIButton;
+    public pauseButton  : UIButton;
+    public thePlayer    : Player;
+    public back10Button  : UIButton;
+
+    constructor(player : Player) {
+        super(null);
+        this.setID("UIPlayerController");
+        this.thePlayer = player;
+    }
+
+    public initialize() {
+        super.initialize();
+        super.defineEvents();
+        this.defineEvents();
+    }
+
+    public getView() : any {
+        this.playButton = new  UIButton({ label : "Play" });
+        this.addComponent("playButton",this.playButton)
+        this.stopButton = new UIButton({ label : "Stop"} );
+        this.addComponent("stopButton",this.stopButton)
+        this.pauseButton = new UIButton({ label : "Pause" })
+        this.addComponent("pauseButton",this.pauseButton);
+
+        this.back10Button = new UIButton({ label : "Back 10" });
+        this.addComponent("back10Button",this.back10Button);
+
+
+        this.componentView = this.createView({
+            id      : this.componentID,
+            view    : "form",
+            rows    : [  this.getComponent("playButton").getView(), this.getComponent("stopButton").getView(), this.getComponent("pauseButton").getView(),this.getComponent("back10Button").getView() ]
+        });
+        return this.componentView;
+    }
+
+    public listen(event, data, caller) {
+
+        switch (event) {
+            case "playVideo" : {
+                this.thePlayer.play();
+            }
+                break;
+            case "stopButton" : {
+                this.thePlayer.stop();
+            }
+                break;
+            case "pauseButton": {
+                this.thePlayer.pause();
+            }
+                break;
+            case "back10Button": {
+                this.thePlayer.setTime( this.thePlayer.getTime()-10);
+            }
+                break;
+        }
+    }
+
+    public defineEvents() {
+        this.getComponent("playButton").subscribe("click",this,"playVideo");
+        this.getComponent("stopButton").subscribe("click",this,"stopButton");
+        this.getComponent("pauseButton").subscribe("click",this,"pauseButton");
+        this.getComponent("back10Button").subscribe("click",this,"back10Button");
+
+    }
+
+} this.UIPlayerControls = UIPlayerControls;
+
+class FabricPlayer extends Player {
+
+    public static canvas : any;
+    public videoElement : any;
+    public static videoPlayer : VideoPlayer;
+    public videoObject : any;
+    public request : any;7
+
+    constructor( ) {
+        super();
+    }
+
+    public setCanvas(element : string) {
+
+        FabricPlayer.canvas = new fabric.Canvas(element);
+    }
+
+    public setVideo(videoPlayer : VideoPlayer) {
+
+        FabricPlayer.videoPlayer = videoPlayer;
+
+        var videoEL = document.getElementById("videoDiv");
+
+        //  this.videoObject = new fabric.Image(FabricPlayer.videoPlayer.videoElement);
+        this.videoObject = new fabric.Image(videoEL);
+
+        this.videoElement = FabricPlayer.videoPlayer.videoElement;
+        FabricPlayer.canvas.add(this.videoObject);
+    }
+
+
+    public static render() {
+        FabricPlayer.canvas.renderAll();
+        var request = fabric.util.requestAnimFrame(FabricPlayer.render);
+        var current_time = FabricPlayer.videoPlayer.currentTime();
+        if(current_time >= 100) {
+            FabricPlayer.videoPlayer.pause();
+            FabricPlayer.cancelRequestAnimFrame(request);
+        }
+
+        AnnotationBlock.theBlock.play(current_time);
+
+        //console.log(current_time);
+    }
+
+    public play() {
+        this.videoObject.getElement().play();
+        FabricPlayer.videoPlayer.play();
+        fabric.util.requestAnimFrame(FabricPlayer.render);
+    }
+
+
+
+    public pause() {
+
+        this.videoObject.getElement().pause();
+        FabricPlayer.videoPlayer.pause();
+
+        FabricPlayer.cancelRequestAnimFrame(this.request);
+
+    }
+    public drawCircle() {
+
+        FabricPlayer.canvas.item(0).hasBorders = false;
+        FabricPlayer.canvas.setActiveObject(FabricPlayer.canvas.item(0));
+    }
+
+    public currentTime() : number {
+        var current_time = this.videoElement.currentTime;
+        return current_time;
+    }
+
+    public getTime() : number {
+        return this.currentTime();
+    }
+    public setTime(time : number) {
+        if (time < 0) time = 0;
+        this.videoObject.getElement().currentTime = time;
+    }
+
+
+    public static cancelRequestAnimFrame(request : any) {
+        return window.cancelAnimationFrame ||
+            window.webkitCancelRequestAnimationFrame ||
+            window.mozCancelRequestAnimationFrame ||
+            window.oCancelRequestAnimationFrame ||
+            window.msCancelRequestAnimationFrame ||
+            clearTimeout;
+    }
+
+}  this.FabricPlayer = FabricPlayer;
--- typescript-defs/uicore.d.ts
+++ typescript-defs/uicore.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="../client/AB_Gui.ts"/>
 /// <reference path="../client/AA_ObjectUpdateRouter.ts"/>
 /// <reference path="../client/CC_UIConsistencyChecker.ts"/>
-/// <reference path="../client/BB_UIVideoPlayer.ts"/>
+/// <reference path="../client/BB_VideoPlayer.ts"/>
 /// <reference path="../client/FabricPlayer.ts"/>
